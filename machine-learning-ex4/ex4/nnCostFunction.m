1f12a532240684bc159fdb9af0effa0ede7d8907
Balázs Kiss <dev@balazskiss.name>
2021-03-17 10:55:35 +0100

    ex4 feedforward cost function

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -64,7 +64,23 @@ Theta2_grad = zeros(size(Theta2));
 
 
 
+% Part 1
 
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+h = a3;
+
+I = eye(num_labels);
+Y = zeros(m, num_labels);
+for i = 1:m
+  Y(i, :)= I(y(i), :);
+end
+
+p = sum(sum(Theta1(:, 2:end) .^ 2, 2)) + sum(sum(Theta2(:, 2:end) .^ 2, 2));
+J = sum(sum((-Y) .* log(h) - (1 - Y) .* log(1-h), 2)) / m + lambda * p / (2 * m);
 
 
 

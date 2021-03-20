e11952b3b26ac08f8e0e5feaf449ccec31867baf
Balázs Kiss <dev@balazskiss.name>
2021-03-20 10:59:27 +0100

    ex5 learning curve

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,11 +53,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
-
-
-
+for i = 1:m
+    [theta] = trainLinearReg(X(1:i, :), y(1:i), lambda);
+    [Jt, _] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+    error_train(i) = Jt;
+    [Jv, _] = linearRegCostFunction(Xval, yval, theta, 0);
+    error_val(i) = Jv;
+end
 
 % -------------------------------------------------------------
 

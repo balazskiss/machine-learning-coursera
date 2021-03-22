dab98f16ccece232866cc21da8dff777c5a382d5
Balázs Kiss <dev@balazskiss.name>
2021-03-22 11:00:48 +0100

    ex6 dataset params

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+CList = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmaList = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+error = 10000;
 
-
-
-
+for i = 1:length(CList),
+    currentC = CList(i);
+    for j = 1:length(sigmaList),
+    	currentSigma = sigmaList(j);
+		model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+		predictions = svmPredict(model, Xval);
+		currentError = mean(double(predictions ~= yval));
+		if currentError < error,
+		    error = currentError;
+		    C = currentC;
+		    sigma = currentSigma;
+		end
+	end
+end
 
 % =========================================================================
 
